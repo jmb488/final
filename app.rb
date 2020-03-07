b5f9416ced6a64e6faa14d52bbba2ab8d50b7943
jmb488 <jeff.barker@kellogg.northwestern.edu>
2020-03-07 18:04:46 +0000

    initial setup

--- app.rb
+++ app.rb
@@ -14,5 +14,115 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+neighborhoods_table = DB.from(:neighborhoods)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+
+  #  results = Geocoder.search(@params["q"])
+   # @lat_long = results.first.coordinates  # gives [lat, long]
+
+
+get "/" do
+    puts neighborhoods_table.all
+    @neighborhoods = neighborhoods_table.all.to_a
+    view "neighborhoods"
+end
+
+get "/neighborhoods/:id" do
+    @neighborhood = neighborhoods_table.where(id: params[:id]).to_a[0]
+    @reviews = reviews_table.where(neighborhood_id: @neighborhood[:id])
+    @review_count = reviews_table.where(neighborhood_id: @neighborhood[:id]).count(:id)
+
+    @avg_affordability_sat = reviews_table.where(neighborhood_id: @neighborhood[:id]).avg(:affordability_sat)
+    @avg_public_transit_sat = reviews_table.where(neighborhood_id: @neighborhood[:id]).avg(:public_transit_sat)
+    @avg_bike_walk_sat = reviews_table.where(neighborhood_id: @neighborhood[:id]).avg(:bike_walk_sat)
+    @avg_safety_sat = reviews_table.where(neighborhood_id: @neighborhood[:id]).avg(:safety_sat)
+    @avg_thing_to_do_sat = reviews_table.where(neighborhood_id: @neighborhood[:id]).avg(:thing_to_do_sat)
+    @avg_overall_sat = reviews_table.where(neighborhood_id: @neighborhood[:id]).avg(:overall_sat)
+
+    @avg_studio_rent = reviews_table.where(neighborhood_id: @neighborhood[:id], unit_type: "Studio").avg(:monthly_rent)
+    @avg_1bd_rent = reviews_table.where(neighborhood_id: @neighborhood[:id], unit_type: "One Bedroom").avg(:monthly_rent)
+    @avg_2bd_rent = reviews_table.where(neighborhood_id: @neighborhood[:id], unit_type: "Two Bedroom").avg(:monthly_rent)
+    @avg_3bd_rent = reviews_table.where(neighborhood_id: @neighborhood[:id], unit_type: "Three Bedroom").avg(:monthly_rent)
+
+    @users_table = users_table
+    view "neighborhood"
+end
+
+get "/neighborhoods/:id/reviews/new" do
+    @neighborhood = neighborhoods_table.where(id: params[:id]).to_a[0]
+    view "new_review"
+end
+
+get "/events/:id/reviews/create" do
+    puts params
+    @neighborhood = neighborhoods_table.where(id: params[:id]).to_a[0]
+    reviews_table.insert(
+                    neighborhood_id: params["id"],
+                    user_id: session["user_id"],
+                    affordability_sat: params["affordability_sat"],
+                    public_transit_sat: params["public_transit_sat"],
+                    bike_walk_sat: params["bike_walk_sat"],
+                    safety_sat: params["safety_sat"],
+                    thing_to_do_sat: params["thing_to_do_sat"],
+                    overall_sat: params["overall_sat"],
+                    comment: params["comment"],
+                    unit_type: params["unit_type"],
+                    monthly_rent: params["monthly_rent"],
+    )
+    view "create_review"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+get "/users/create" do
+    puts params
+    users_table.insert(
+            name: params["name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+    )
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    puts params
+    # If the name = a name in the DB
+    # AND the password matches the password for that name
+    # then - create_login, else login failed
+
+    email_address = params["email"]
+    password = params["password"]
+
+    @user = users_table.where(email: email_address).to_a[0]
+
+    if @user
+        if BCrypt::Password.new(@user[:password]) == password
+            puts @user
+            session["user_id"] = @user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    view "logout"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,124 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
-  primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
+DB.create_table! :neighborhoods do
+    primary_key :id
+    String :name
+    String :city
+    Integer :density
+    Integer :income
 end
-DB.create_table! :rsvps do
-  primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
-  String :email
-  String :comments, text: true
+DB.create_table! :reviews do
+    primary_key :id
+    foreign_key :neighborhood_id
+    foreign_key :user_id
+    Integer :affordability_sat
+    Integer :public_transit_sat
+    Integer :bike_walk_sat
+    Integer :safety_sat
+    Integer :thing_to_do_sat
+    Integer :overall_sat
+    String :comment, text: true
+    String :unit_type
+    Integer :monthly_rent
+end
+DB.create_table! :users do
+    primary_key :id
+    String :name
+    String :email
+    String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+neighborhoods_table = DB.from(:neighborhoods)
+reviews_table = DB.from(:reviews)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+neighborhoods_table.insert(name: "Lincoln Park", 
+                    city: "Chicago",
+                    density: 21322,
+                    income: 100624)
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+
+neighborhoods_table.insert(name: "Lakeview East", 
+                    city: "Chicago",
+                    density: 40266,
+                    income: 66564)
+
+
+neighborhoods_table.insert(name: "Uptown", 
+                    city: "Chicago",
+                    density: 20984,
+                    income: 37609)
+
+
+neighborhoods_table.insert(name: "West Loop", 
+                    city: "Chicago",
+                    density: 21644,
+                    income: 107626)
+
+
+neighborhoods_table.insert(name: "Wicker Park", 
+                    city: "Chicago",
+                    density: 21742,
+                    income: 95586)
+
+            
+neighborhoods_table.insert(name: "Logan Square", 
+                    city: "Chicago",
+                    density: 25602,
+                    income: 53980)
+
+
+neighborhoods_table.insert(name: "Albany Park", 
+                    city: "Chicago",
+                    density: 31880,
+                    income: 53349)
+
+
+neighborhoods_table.insert(name: "South Loop", 
+                    city: "Chicago",
+                    density: 10765,
+                    income: 80940)
+
+
+neighborhoods_table.insert(name: "Pilsen", 
+                    city: "Chicago",
+                    density: 14411,
+                    income: 37308)
+
+
+neighborhoods_table.insert(name: "Bridgeport", 
+                    city: "Chicago",
+                    density: 13199,
+                    income: 44968)
+
+
+
+reviews_table.insert(
+                    neighborhood_id:1,
+                    user_id:0,
+                    affordability_sat:4,
+                    public_transit_sat:4,
+                    bike_walk_sat:4,
+                    safety_sat:4,
+                    thing_to_do_sat:4,
+                    overall_sat:4,
+                    comment:"Great!",
+                    unit_type:"Studio",
+                    monthly_rent:1500
+)
+
+
+reviews_table.insert(
+                    neighborhood_id:1,
+                    user_id:1,
+                    affordability_sat:5,
+                    public_transit_sat:5,
+                    bike_walk_sat:5,
+                    safety_sat:5,
+                    thing_to_do_sat:5,
+                    overall_sat:5,
+                    comment:"Great!",
+                    unit_type:"Studio",
+                    monthly_rent:1300
+)
